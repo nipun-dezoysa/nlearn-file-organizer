3f3a4d72d97647fb1e4173b57ab55cc6cbb99c68
nipun-dezoysa <nipunavishka123@gmail.com>
2024-08-01 13:59:48 +0530

    files categorizer

--- background.js
+++ background.js
@@ -1,21 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "download") {
-    chrome.downloads.download({ url: request.url }, (downloadId) => {
-      if (downloadId) {
-        console.log(`Download initiated with ID: ${downloadId}`);
-      } else {
-        console.error(`Download failed: ${chrome.runtime.lastError}`);
+    chrome.downloads.download(
+      {
+        url: request.url,
+        filename: `nlearn/${request.course}/${request.fileName}`,
+      },
+      (downloadId) => {
+        if (downloadId) {
+          console.log(`Download initiated with ID: ${downloadId}`);
+        } else {
+          console.error(`Download failed: ${chrome.runtime.lastError}`);
+        }
       }
-    });
-  }
-});
-
-chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
-  if (item.url.startsWith("https://nlearn.nsbm.ac.lk/mod/resource")) {
-    // Specify the download path
-    const downloadPath = `nlearn/${item.filename}`;
-    console.log(`Suggesting filename: ${downloadPath}`);
-    // Suggest the new filename
-    suggest({ filename: downloadPath });
+    );
   }
 });
--- content.js
+++ content.js
@@ -9,9 +9,27 @@ document.querySelectorAll("a").forEach((link) => {
       chrome.runtime.sendMessage({
         action: "download",
         url: link.getAttribute("href"),
+        course: document.title.substring(8, document.title.length),
+        fileName: getFileName(link),
       });
     } else {
       chrome.tabs.create({ url: link.getAttribute("href") });
     }
   });
 });
+
+function getFileName(element) {
+  var fileName = element.lastChild.textContent.substring(
+    0,
+    element.lastChild.textContent.length - 5
+  );
+  var src = element.firstChild.getAttribute("src");
+  if (src.includes("powerpoint")) {
+    fileName += ".pptx";
+  } else if (src.includes("pdf")) {
+    fileName += ".pdf";
+  } else if (src.includes("document")) {
+    fileName += ".docx";
+  }
+  return fileName;
+}
